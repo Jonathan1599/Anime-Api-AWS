7b823fb11e566d0e486d36ae0ec2f633e4071f0a
Jonathan D'mello <jonathandmello@yahoo.co.in>
2021-08-28 20:37:26 +0530

    Web server init with gorilla mux

--- main.go
+++ main.go
@@ -1,9 +1,36 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    //"log"
+    "net/http"
+	"github.com/gorilla/mux"
+)
 
+func AnimeHandler(w http.ResponseWriter, r *http.Request){
+	vars := mux.Vars(r)
+	id, ok := vars["id"]
+	fmt.Println(ok)
+	if !ok {
+		fmt.Println("id is missing in parameters")
+	}
 	
+	// Web scraping
 
-func main() {
-    fmt.Println("hello world")
+    fmt.Fprintf(w, id)
+    fmt.Println("Endpoint Hit: AnimeHandler")
+}
+
+func AnimePathErrorHandler(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "Please enter an id")
+	fmt.Println("Endpoint Hit: AnimePathErrorHandler")
+}
+
+
+func main(){
+	r := mux.NewRouter()
+	r.HandleFunc("/anime/{id}", AnimeHandler).Methods("GET")
+	r.HandleFunc("/anime", AnimePathErrorHandler).Methods("GET")
+
+	http.ListenAndServe(":3000", r)
 }
